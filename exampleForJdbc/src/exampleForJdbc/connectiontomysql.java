c5a96a422c71e9c755c0215626dfdb8320bc2aa7
pkumpatla <pkumpatla@IN-BN3K9S3>
2024-02-26 12:54:46 +0530

    First commit

---
+++ exampleForJdbc/src/exampleForJdbc/connectiontomysql.java
@@ -0,0 +1,41 @@
+package exampleForJdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class connectiontomysql {
+
+	public static void main(String[] args) throws Exception{
+		String url ="jdbc:mysql://localhost:3306/csk";
+		String uname ="root";
+		String pass="sekhar1126";
+		String query = "select  name from students_list where roll_no=1";
+//Class.forName("com.mysql.cj.jdbc.Driver");
+     Connection con = DriverManager.getConnection(url,uname,pass);
+     if (con==null) {
+      System.out.println("not connected");
+ 
+     }else {
+	System.out.println(" connected");	
+
+     }
+
+     Statement st = con.createStatement();
+
+     ResultSet rs = st.executeQuery(query);
+
+     rs.next();
+
+     String name = rs.getString("name");
+
+     System.out.println(name);
+
+     st.close();
+
+     con.close();
+ 
+	
+	}
+}
---
+++ exampleForJdbc/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module exampleForJdbc {
+	requires java.sql;
+}
